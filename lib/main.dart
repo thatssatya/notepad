48f75db31c6fcc6e069ffe2d2955cf18688661ab
thatssatya <d.coder.rsatya@gmail.com>
2020-06-16 15:37:30 +0530

    starting afresh

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('NotePad'),
-      ),
+    return MaterialApp(
     );
   }
 }
-
